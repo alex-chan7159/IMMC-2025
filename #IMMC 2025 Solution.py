63b1d267a1d5587aa4066a4b0c5ffdbed47a95c5
alex_chan_7159 <115313791+alex-chan7159@users.noreply.github.com>
2025-03-24 18:55:06 -0400

    Update #IMMC 2025 Solution.py

--- #IMMC 2025 Solution.py
+++ #IMMC 2025 Solution.py
@@ -1,1 +1,11 @@
-#IMMC 2025 Solution
+# IMMC 2025 Solution (Schedule Implementation)
+
+# Teams from the same continent face each other
+# Factors in team strength (strong team more likely to win)
+# Matchmaking based on performance
+# No team faces each other twice
+# Each team gets 5 home games and 5 away games
+# Tiebreaker score to decide winner if tied for # of wins
+#   Should be the sum of the FIBA rankings of the opponents they have faced in all of their rounds combined, where the lower it is the better
+
+print("holy guacamole")
