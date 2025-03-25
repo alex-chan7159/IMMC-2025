1aec4563cb04687b7dbbc4759bbcf20b89e7771e
alex_chan_7159 <115313791+alex-chan7159@users.noreply.github.com>
2025-03-24 21:00:33 -0400

    Update #IMMC 2025 Solution.py
    Team object

--- #IMMC 2025 Solution.py
+++ #IMMC 2025 Solution.py
@@ -8,4 +8,13 @@
 # Tiebreaker score to decide winner if tied for # of wins
 #   Should be the sum of the FIBA rankings of the opponents they have faced in all of their rounds combined, where the lower it is the better
 
-print("holy guacamole")
+class Team:
+    def __init__(self, country, points, teamsPlayed, fibaRank, homeLocation, games):
+        self.country = country #string
+        self.points = points #int, victory is worth 1 while draws are worth 0?
+        self.teamsPlayed = teamsPlayed #{}, dynamic list
+        self.fibaRank = fibaRank #int
+        self.homeLocation = homeLocation #{latitude, longitude}
+        #self.games = games #{wins, draws, losses}
+
+
